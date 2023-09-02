b1af429ed738cee6086764f7ca91235c7ff970e4
Ahmed_AbdelKader <133119727+Abdelkader71@users.noreply.github.com>
2023-09-02 18:27:34 +0300

    Buuton grids

--- task1.py
+++ task1.py
@@ -1,20 +1,29 @@
-bits = [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0]
-for i,bit in enumerate(bits):
-    try:
-        bit = int(input(f"Please enter bit {i} mode: "))
-        if bit in range(0 , 2):
-            bits[i] = str(bit)
-        else:
-            bit = int(input(f"Undefined mode.Please enter bit {i} mode again: "))
-    except ValueError:
-        bit = int(input(f"Invalid input. Please enter the bit {i} mode in number 0 for Input, 1 for Output: "))
-        if bit in range(0 , 2):
-            bits[i] = str(bit)
-        else:
-            bit = int(input(f"Undefined mode.Please enter bit {i} mode again: "))
-num ="".join(bits)
-file = open("file.c" , "a")
-file.write("void Init_voidPortADir (void)\n")
-file.write("{")
-file.write("\n\tDDRA = 0b"+num+";\n")
-file.write("}")
+from tkinter import *
+
+def button1():
+    print("Button 1 is pressed")
+def button2():
+    print("Button 2 is pressed")
+def button3():
+    print("Button 3 is pressed")
+def button4():
+    print("Button 4 is pressed")
+
+window = Tk()
+window.title("Hello world")
+window.geometry("200x200+500+400")
+window.resizable(False,False)
+
+#buttons handler
+bt1 = Button(window , text="Button1" , fg="red" , bg="black" , command=button1)
+bt2 = Button(window , text="Button2" , fg="red" , bg="black" , command=button2)
+bt3 = Button(window , text="Button3" , fg="red" , bg="black" , command=button3)
+bt4 = Button(window , text="Button4" , fg="red" , bg="black" , command=button4)
+
+#place
+bt1.grid(row=0,column=1)
+bt2.grid(row=1,column=0)
+bt3.grid(row=1,column=2)
+bt4.grid(row=2,column=1)
+
+window.mainloop()
